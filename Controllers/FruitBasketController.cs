5cf4114db0bb9ae9e0ac3d7c93bf535327f89df9
ArshadMajgaonkar-GEP <Arshad.Majgaonkar@gep.com>
2025-05-10 12:48:34 +0530

    Add project files.

---
+++ Controllers/FruitBasketController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitBasketController : ControllerBase
+    {
+
+        static List<string> fruitBasket = new List<string> { "Apple", "Banana", "Cherry" };
+
+        [HttpGet("GetFruits")]
+        public IActionResult GetFruitBasket()
+        {
+            return Ok(fruitBasket);
+        }
+
+        [HttpPost("AddToBasket")]
+        public IActionResult AddFruit([FromBody] string name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                fruitBasket.Add(name);
+                return Ok(fruitBasket);
+            }
+            else
+            {
+                return BadRequest("Fruit name cannot be empty.");
+            }
+        }
+    }
+}
